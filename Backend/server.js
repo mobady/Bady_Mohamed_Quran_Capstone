f27a1982688bb1c5048e87469cfe6ac8757bf47a
mobady <mohamedmahmoud.bady@icloud.com>
2024-10-09 20:48:48 -0400

    create a router that gets all ayhas form database and sends to the user Frontend

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,7 @@ import cors from "cors"
 import "dotenv/config"
 import connectDB from "./config.js"
 import Quran from "./models/QuranModel.js"
-import ayah from "./data.js"
+
 
 import Ayah from "./models/AyahModel.js"
 
@@ -11,32 +11,18 @@ const app = express()
 const PORT = 8080
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.json('hello world')
-})
 
-app.get('/quran', async (req,res) =>{
+app.get('/ayah', async (req,res) =>{
     try {
-        const quran = await Quran.find({})
+        const ayahs = await Ayah.find({})
         console.log("Get res from /quran")
-        res.status(200).json(quran)
+        res.status(200).json(ayahs)
     } catch (error) {
         console.log(error)
         res.status(400).json(error)
     }
 })
 
-
-app.get('/ayah', async (req,res) =>{
-    try {
-
-      await Ayah.insertMany(ayah)
-      console.log('add data to my database')
-        
-    } catch (error) {
-        console.log(error)
-    }
-})
 
 app.listen(PORT, () =>{
     console.log("Listening on port: " + PORT)
